2db5a4ca38906ff15c263b8a6928ee54a7772a1d
Francisco Sandoval <francisco.sandoval@newfront.com>
2021-05-06 11:20:30 -0400

    feat: add custom validation pipe for task status

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,13 @@
+import { PipeTransform, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { TaskStatus } from '../tasks.model';
+
+const validTaskStatues = Object.values(TaskStatus) as TaskStatus[];
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata): any {
+    if (!validTaskStatues.includes(value)) {
+      throw new BadRequestException(`${value} is not a valid task status`);
+    }
+    return value;
+  }
+}
