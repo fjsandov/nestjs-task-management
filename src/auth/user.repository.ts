19f9f07d13e6a0f74de72f1f27f5d10116fd7d11
Francisco Sandoval <francisco.sandoval@newfront.com>
2021-05-21 11:10:44 -0400

    feat: add auth module with signup

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,37 @@
+import { EntityRepository, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+enum ErrorCodes {
+  DUPLICATE_USERNAME_CODE = '23505',
+}
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = new User();
+    user.username = username;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === ErrorCodes.DUPLICATE_USERNAME_CODE) {
+        throw new ConflictException('Username already exists');
+      }
+      throw new InternalServerErrorException();
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
+  }
+}
